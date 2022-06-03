6554b8bc6025479be4d89c81fd10617dd96d5b37
rez-spb <rez.open@gmail.com>
2022-06-03 16:29:19 +0300

    XHTML improve.

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def printer_console(users):
 
 
 def printer_html(users, fd):
-    print("""
-<?xml version="1.0" encoding="UTF-8"?>
+    lines = []
+    header = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
@@ -43,25 +43,17 @@ def printer_html(users, fd):
     <body>
         <h1>Steam Group User List</h1>
         <table>
-            <tr><th>#</th><th>avatar</th><th>name</th><th>id64</th></tr>
-""", file=fd)
-
+            <tr><th>#</th><th>avatar</th><th>name</th><th>id64</th></tr>\n"""
+    footer = "\n</table></body></html>"
+    row_template = "<tr><td>{num:02d}</td>" \
+                   "<td><img alt='{id64}' src='{avatar}' /></td>" \
+                   "<td>{user}{url}</td><td>{id64}</td></tr>"
     for n, u_obj in enumerate(users, start=1):
-        print("<tr>", file=fd)
-        print(f"<td>{n:02d}</td>", file=fd)  # #
-        print(f"<td><img src='{u_obj.avatar}' /></td>", file=fd)  # avatar
-        print(f"<td>{u_obj.username}", file=fd)  # name
-        if u_obj.custom_url:
-            print(f" ({u_obj.custom_url})", file=fd)
-        print("</td>", file=fd)
-        print(f"<td>{u_obj.id64}</td>", file=fd)  # id64
-        print("</tr>", file=fd)
-
-    print(
-        """
-        </table>
-    </body>
-</html>""", file=fd)
+        lines.append(row_template.format(
+            num=n, avatar=u_obj.avatar, user=u_obj.username,
+            url=f" ({u_obj.custom_url})" if u_obj.custom_url else '',
+            id64=u_obj.id64))
+    print(header + '\n'.join(lines) + footer, file=fd)
 
 
 for user in group_users:
